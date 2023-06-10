a87276c746bfaf55e05f4a411ff62e75624a4b31
Jan Mewes <jan.mewes@ks-plus.org>
2023-06-10 07:17:18 +0200

    feat: generate import statements

--- src/main/java/com/experimental_software/api_generator/code_generation/ClassFinder.java
+++ src/main/java/com/experimental_software/api_generator/code_generation/ClassFinder.java
@@ -1,6 +1,7 @@
 package com.experimental_software.api_generator.code_generation;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.ClassPath;
@@ -8,27 +9,39 @@ import com.google.common.reflect.ClassPath;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@SuppressWarnings("UnstableApiUsage")
 public class ClassFinder {
 
     private final String simpleClassName;
 
-    /**
-     * Returns the import path of the class e.g. "org.openehr.rm_data_types.quantity.DvCount".
-     *
-     * @see <a href="https://www.baeldung.com/jvm-list-all-classes-loaded">List All the Classes Loaded in the JVM | baeldung.com</a>
-     * @see <a href="https://stackoverflow.com/questions/3222638/get-all-of-the-classes-in-the-classpath">Get all of the Classes in the Classpath | stackoverflow.com</a>
-     */
-    @SuppressWarnings("UnstableApiUsage")
-    public String getImport() {
+    @SuppressWarnings("rawtypes")
+    public Optional<? extends Class> getClassRepresentation() {
         return getAllOpenEhrClasses().stream()
-            .map(ClassPath.ClassInfo::getName)
-            .filter(name -> name.endsWith(simpleClassName))
-            .findFirst()
-            .orElseThrow();
+            .filter(c -> c.getName().endsWith("." + simpleClassName))
+            .map(ClassPath.ClassInfo::load)
+            .findFirst();
     }
     // TODO: If running in strict mode, raise exception if multiple classes are found.
 
-    @SuppressWarnings("UnstableApiUsage")
+    /**
+     * Returns the import path of the class e.g. "org.openehr.rm_data_types.quantity.DvCount".
+     */
+    public Optional<String> getImport() {
+        return getAllOpenEhrClasses().stream()
+            .map(ClassPath.ClassInfo::getName)
+            .filter(name -> name.endsWith("." + simpleClassName))
+            .findFirst();
+    }
+    // TODO: If running in strict mode, raise exception if multiple classes are found.
+
+    /**
+     * @see <a href="https://www.baeldung.com/jvm-list-all-classes-loaded">
+     *     List All the Classes Loaded in the JVM | baeldung.com
+     *     </a>
+     * @see <a href="https://stackoverflow.com/questions/3222638/get-all-of-the-classes-in-the-classpath">
+     *     Get all of the Classes in the Classpath | stackoverflow.com
+     *     </a>
+     */
     private static ImmutableSet<ClassPath.ClassInfo> getAllOpenEhrClasses() {
         var cl = ClassFinder.class.getClassLoader();
         try {
--- src/test/java/com/experimental_software/api_generator/code_generation/ClassFinderTest.java
+++ src/test/java/com/experimental_software/api_generator/code_generation/ClassFinderTest.java
@@ -4,13 +4,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
+import org.openehr.rm_data_types.quantity.DvCount;
 
 public class ClassFinderTest {
 
     @Test
     public void testInferImportPath() {
-        var result = new ClassFinder("DvCount").getImport();
+        var result = new ClassFinder("DvCount").getImport().orElseThrow();
 
         assertThat(result, is("org.openehr.rm_data_types.quantity.DvCount"));
+    }
+
+    @Test
+    public void testInferClassObject() {
+        var result = new ClassFinder("DvCount").getClassRepresentation().orElseThrow();
+
+        assertThat(result, is(DvCount.class));
     }
 }
